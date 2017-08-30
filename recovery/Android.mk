03b13e77c4b5aee81ed947e5d316c87e36bbd48c
unos21 <sonukr22@gmail.com>
2017-08-30 16:04:24 +0530

    update for o

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -6,7 +6,8 @@ ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := bootable/recovery \
+    bootable/recovery/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_ham
 LOCAL_MODULE_TAGS := eng
